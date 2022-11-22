12c6a2e682f7aa64e4fc6af14094edbe111e1e73
nikonovdmitiy <nikonov9094@gmail.com>
2022-11-22 09:21:00 +0500

    Initial commit

---
+++ java/controllers/BucketController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import models.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import services.BucketService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/order")
+public class BucketController {
+
+    private final BucketService bucketService;
+
+    @Autowired
+    public BucketController(BucketService bucketService) {
+        this.bucketService = bucketService;
+    }
+
+    @GetMapping("/get")
+    public Collection<Item> getItems() {
+        return bucketService.getItems();
+    }
+
+    @GetMapping("/add")
+    public void addItem(@RequestParam(name = "item", required = false) int[] ids) {
+        bucketService.addItems(ids);
+    }
+
+}
